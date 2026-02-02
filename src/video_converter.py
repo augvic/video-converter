2b780fa321e95c5f7bce2f4694a8cf8e4a0d63af
AugustoViceli <a.viceli7357@gmail.com>
2026-02-02 14:14:14 -0300

    Added storage folder auto-create.

--- src/video_converter.py
+++ src/video_converter.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+from os import makedirs
+
 from .engines.engines import Engines
 from .convert_task import ConvertTask
 from .cli import Cli
@@ -5,6 +9,11 @@ from .cli import Cli
 class VideoConverter:
     
     def __init__(self) -> None:
+        if getattr(sys, "frozen", False):
+            storage_path = Path(sys.executable).parent / "storage"
+        else:
+            storage_path = Path(__file__).resolve().parents[3] / "storage"
+            makedirs(storage_path, exist_ok=True)
         self.engines = Engines()
         self.converter_task = ConvertTask(self.engines)
         self.cli = Cli(self.converter_task)
